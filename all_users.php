16ce6ac48360d2667ae4e7c7f1fc8c064de7ffa8
LBCHTS <loic.beauchaints@iut-rodez.fr>
2019-11-13 10:32:52 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non préparé

--- all_users.php
+++ all_users.php
@@ -2,30 +2,52 @@
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  //<link rel="stylesheet" href="css/styles.css?v=1.0">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
   <title>all_users</title>
 </head>
 <body>
     <h1>All Users</h1>
+    <table>
     <?php
-        $host = 'localhost';
-        $db   = ‘my-activities’;
-        $user = 'root';
-        $pass = 'root';
-        $charset = 'utf8mb4';
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $options = [     
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,     
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,     
-            PDO::ATTR_EMULATE_PREPARES   => false, ];
-            try {      
-                $pdo = new PDO($dsn, $user, $pass, $options);
-            } catch (PDOException $e) {     
-                throw new PDOException($e->getMessage(), (int)$e->getCode());
+        $host = 'localhost';
+        $port = '3306';
+        $db   = 'my_activities';
+        $user = 'root';
+        $pass = 'root';
+        $charset = 'utf8mb4';  
+        
+        $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        
+        try {
+            $pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+
+            echo '<tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Status</th>
+                  </tr>';
+            
+            
+            $stmt = $pdo->query('SELECT * FROM users JOIN status ON users.status_id = status.id ORDER BY username');
+            while ($row = $stmt->fetch()) {
+                echo '<tr>';
+                echo '<td>'.$row['id'].'</td>';
+                echo '<td>'.$row['username'].'</td>';
+                echo '<td>'.$row['email'].'</td>';
+                echo '<td>'.$row['name'].'</td>';
+                echo '</tr>';
             }
 
-         // TODO faire l'affichage du tableau
-
     ?>
+    </table>
 </body>
 </html>
