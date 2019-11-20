1a067c68fd09d616437ada73747cd82aa66f35bd
LBCHTS <loic.beauchaints@iut-rodez.fr>
2019-11-20 09:25:19 +0100

    réorganisation des fichiers git

--- all_users.php
+++ all_users.php
@@ -4,6 +4,12 @@
   <meta charset="utf-8">
   <link rel="stylesheet" href="css/styles.css?v=1.0">
   <title>all_users</title>
+  <style type="text/css">
+    table,th,td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+  </style>
 </head>
 <body>
     <h1>All Users</h1>
@@ -28,16 +34,38 @@
         } catch (PDOException $e) {
             throw new PDOException($e->getMessage(), (int)$e->getCode());
         }
+        
+    ?>
+    
+    <form action="all_users.php" method="post"> 
+        <label for="pattern">Start with letter : </label>
+        <input type="search" id="pattern" name="pattern">
 
-            echo '<tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                    <th>Email</th>
-                    <th>Status</th>
-                  </tr>';
-            
-            
-            $stmt = $pdo->query('SELECT * FROM users JOIN status ON users.status_id = status.id ORDER BY username');
+        <label for="status">and status is : </label>
+        <select name="status">
+        <?php
+            $typeStatus = $pdo->query("SELECT * FROM status WHERE id < 3");
+            while ($row = $typeStatus->fetch()) {
+                echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+            }    
+        ?>
+        </select>
+
+        <input type="submit" value="ok">
+    </form>
+
+    <?php
+        echo '<tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Status</th>
+            </tr>';
+
+        /*TODO que ça fonctionne*/
+        if (isset($_POST['pattern']) && isset($_POST['status'])) {
+            $stmt = $pdo->query("SELECT * FROM users JOIN status ON users.status_id = status.id 
+                                WHERE status.name = '$_POST['status']' AND username LIKE '".$_POST['pattern']."%'ORDER BY username");            
             while ($row = $stmt->fetch()) {
                 echo '<tr>';
                 echo '<td>'.$row['id'].'</td>';
@@ -46,7 +74,7 @@
                 echo '<td>'.$row['name'].'</td>';
                 echo '</tr>';
             }
-
+        }
     ?>
     </table>
 </body>
--- dut-info2-premierspaspdo-LBCHTS/README.md
+++
@@ -1,9 +0,0 @@
-# INFO2-PremiersPasPDO
-
-### NOM : BEAUCHAINTS
-### Prénom : Loïc
-### Groupe de TP : 
-- [X] TD1
-- [ ] TD2
-
-#DONE
