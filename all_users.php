97442a72c0a3f1c578e6c93f636e03855b15999b
LBCHTS <loic.beauchaints@iut-rodez.fr>
2019-11-27 10:12:23 +0100

    fix #4 Utilisation d’énoncés préparés

--- all_users.php
+++ all_users.php
@@ -1,10 +1,9 @@
-<!doctype html>
-<html lang="fr">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
-  <title>all_users</title>
-  <style>
+    <meta charset="UTF-8">
+    <title>All users</title>
+    <style>
         table {
             border-collapse: collapse;
             width: 100%;
@@ -17,75 +16,67 @@
     </style>
 </head>
 <body>
-    <h1>All Users</h1>
-    <table>
-    <?php
-        $host = 'localhost';
-        $port = '3306';
-        $db   = 'my_activities';
-        $user = 'root';
-        $pass = 'root';
-        $charset = 'utf8mb4';  
-        
-        $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
-        
-        try {
-            $pdo = new PDO($dsn, $user, $pass, $options);
-        } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), (int)$e->getCode());
-        }
-        
-    ?>
-    
-    <form action="all_users.php" method="post"> 
-        <label for="pattern">Start with letter : </label>
-        <input type="text" id="pattern" name="pattern">
 
-        <label for="status">and status is : </label>
-        <select name="status">
-        <?php
-            $typeStatus = $pdo->query("SELECT * FROM status WHERE id < 3");
-            while ($row = $typeStatus->fetch()) {
-                echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
-            }    
-        ?>
-        </select>
+<?php
+$host = 'localhost';
+$db = 'my_activities';
+$user = 'root';
+$pass = 'root';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    throw new PDOException($e->getMessage(), (int)$e->getCode());
+}
+function get($name) {
+    return isset($_GET[$name]) ? $_GET[$name] : null;
+}
+?>
 
-        <input type="submit" value="ok">
-    </form>
+<h1>All Users</h1>
 
-    <?php
-        echo '<tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Status</th>
-            </tr>';
+<form action="all_users.php" method="get">
+    Start with letter:
+    <input name="start_letter" type="text" value="<?php echo get("start_letter") ?>">
+    and status is:
+    <select name="status_id">
+        <option value="1" <?php if (get("status_id") == 1) echo 'selected' ?>>Waiting for account validation</option>
+        <option value="2" <?php if (get("status_id") == 2) echo 'selected' ?>>Active account</option>
+    </select>
+    <input type="submit" value="OK">
+</form>
 
-        if (isset($_POST['pattern']) && isset($_POST['status'])) {
-            $pattern = '%'.$_POST['pattern'].'%';
-            $status = '='.$_POST['status'];
-        } else {
-            $pattern = '%';
-            $status = '<=3';
-        }
+<?php
+$start_letter = htmlspecialchars(get("start_letter"));
+$status_id = (int)get("status_id");
+$sql = "select users.id as user_id, username, email, s.name as status from users join status s on users.status_id = s.id where username like ? and status_id = ? order by username";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$start_letter,$status_id]);
+?>
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Status</th>
+    </tr>
+    <?php while ($row = $stmt->fetch()) { ?>
+        <tr>
+            <td><?php echo $row['user_id'] ?></td>
+            <td><?php echo $row['username'] ?></td>
+            <td><?php echo $row['email'] ?></td>
+            <td><?php echo $row['status'] ?></td>
+        </tr>
+    <?php } ?>
+</table>
 
-        $stmt = $pdo->query("SELECT * FROM users JOIN status ON users.status_id = status.id 
-                            WHERE status.id $status AND username LIKE '".$pattern."' ORDER BY username");           
-        while ($row = $stmt->fetch()) {
-            echo '<tr>';
-            echo '<td>'.$row['id'].'</td>';
-            echo '<td>'.$row['username'].'</td>';
-            echo '<td>'.$row['email'].'</td>';
-            echo '<td>'.$row['name'].'</td>';
-            echo '</tr>';
-        }
-    ?>
-    </table>
+
 </body>
 </html>
